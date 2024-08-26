c3dba19554197261f6df440b7a13706d5df9a765
FazalMFH <88107012+FazalMFH@users.noreply.github.com>
2024-08-26 18:08:51 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : String, number, boolean, null , undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 34341231231231431431431431n
+
+
+
+// Reference (Non Primitive)
+
+// Array, Object, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "fazal",
+    age: 17,
+}
+
+const Myfunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof heros );
