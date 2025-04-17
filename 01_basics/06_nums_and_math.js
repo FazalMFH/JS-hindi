6ba6a6a5ca4085fe2a826930294b7f43305f5794
FazalMFH <88107012+FazalMFH@users.noreply.github.com>
2025-04-17 16:02:58 +0000

    changes

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -22,8 +22,8 @@ const hundereds = 1000000
 
 // console.log((Math.random()*10) + 1);
 
-const min = 1
-const max = 6
+const min = 10
+const max = 20
 
 console.log(Math.floor(Math.random() * (max - min + 1)))
   
