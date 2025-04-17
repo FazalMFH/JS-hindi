da46d8b267494a6e6c5baae269965ce359d50ea1
FazalMFH <88107012+FazalMFH@users.noreply.github.com>
2025-04-17 16:02:47 +0000

    changees

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,29 @@
+const score = 200
+// console.log(score);
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundereds = 1000000
+// console.log(hundereds.toLocaleString('en-IN'));
+
+
+
+//-----------------------------MATHS---------------------
+
+// console.log(Math.abs(-4));
+
+// console.log((Math.random()*10) + 1);
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1)))
+  
